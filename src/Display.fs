bb6071f8d0f00a161b0f25ba22fa24bcb576a059
Zaid <zaid.naom@gmail.com>
2019-07-21 12:31:22 +0200

    Finish up the display property

--- src/Display.fs
+++ src/Display.fs
@@ -13,7 +13,37 @@ type display() =
     static member inline grid : IDisplayStyle = unbox "grid"
     /// Displays an element as an inline-level block container. The element itself is formatted as an inline element, but you can apply height and width values.
     static member inline inlineBlock : IDisplayStyle = unbox "inline-block"
+    /// Displays an element as an inline-level flex container.
     static member inline inlineFlex : IDisplayStyle = unbox "inline-flex"
+    /// Displays an element as an inline-level grid container
     static member inline inlineGrid : IDisplayStyle = unbox "inline-grid"
+    /// The element is displayed as an inline-level table.
     static member inline inlineTable : IDisplayStyle = unbox "inline-table"
+    /// Let the element behave like a `<li>` element
     static member inline listItem : IDisplayStyle = unbox "list-item"
+    /// Displays an element as either block or inline, depending on context.
+    static member inline runIn : IDisplayStyle = unbox "run-in"
+    /// Let the element behave like a `<table>` element.
+    static member inline table : IDisplayStyle = unbox "table"
+    /// Let the element behave like a <caption> element.
+    static member inline tableCaption : IDisplayStyle = unbox "table-caption"
+    /// Let the element behave like a <colgroup> element.
+    static member inline tableColumnGroup : IDisplayStyle = unbox "table-column-group"
+    /// Let the element behave like a <thead> element.
+    static member inline tableHeaderGroup : IDisplayStyle = unbox "table-header-group"
+    /// Let the element behave like a <tfoot> element.
+    static member inline tableFooterGroup : IDisplayStyle = unbox "table-footer-group"
+    /// Let the element behave like a <tbody> element.
+    static member inline tableRowGroup : IDisplayStyle = unbox "table-row-group"
+    /// Let the element behave like a <td> element.
+    static member inline tableCell : IDisplayStyle = unbox "table-cell"
+    /// Let the element behave like a <col> element.
+    static member inline tableColumn : IDisplayStyle = unbox "table-column"
+    /// Let the element behave like a <tr> element.
+    static member inline tableRow : IDisplayStyle = unbox "table-row"
+    /// The element is completely removed.
+    static member inline none : IDisplayStyle = unbox "none"
+    /// Sets this property to its default value.
+    static member inline initial : IDisplayStyle = unbox "initial"
+    /// Inherits this property from its parent element.
+    static member inline inheritFromParent : IDisplayStyle = unbox "inherit"
