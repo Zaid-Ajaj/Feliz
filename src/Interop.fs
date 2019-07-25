64f141c2b03847f5d8d014eafe5cc42afd1b8ffb
Zaid <zaid.naom@gmail.com>
2019-07-25 11:58:29 +0200

    Internalize IReactApi in it's own namespace

--- src/Interop.fs
+++ src/Interop.fs
@@ -3,20 +3,12 @@ namespace Feliz
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.React
-
-type internal ReactChildren =
-    abstract toArray: ReactElement -> ReactElement seq
-    abstract toArray: ReactElement seq -> ReactElement seq
-
-type internal IReactApi =
-    abstract createElement: comp: obj * props: obj -> ReactElement
-    abstract createElement: comp: obj * props: obj * [<ParamList>] children: ReactElement seq -> ReactElement
-    abstract Children : ReactChildren
+open Feliz.React
 
 
 [<RequireQualifiedAccess>]
 module Interop =
-    let internal reactApi : IReactApi = importDefault "react"
+    let reactApi : IReactApi = importDefault "react"
     let reactElement (name: string) (props: 'a) : ReactElement = import "createElement" "react"
     let inline mkAttr (key: string) (value: obj) : IReactProperty = unbox (key, value)
     let inline mkStyle (key: string) (value: obj) : IStyleAttribute = unbox (key, value)
---
+++ src/ReactTypes.fs
@@ -0,0 +1,14 @@
+namespace Feliz.React
+
+open Fable.React
+open Fable.Core
+open Fable.Core.JsInterop
+
+type ReactChildren =
+    abstract toArray: ReactElement -> ReactElement seq
+    abstract toArray: ReactElement seq -> ReactElement seq
+
+type IReactApi =
+    abstract createElement: comp: obj * props: obj -> ReactElement
+    abstract createElement: comp: obj * props: obj * [<ParamList>] children: ReactElement seq -> ReactElement
+    abstract Children : ReactChildren
