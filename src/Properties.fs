8af758fbc9f5d008c9c1f8fc2038141e6959b925
Zaid <zaid.naom@gmail.com>
2019-08-05 03:15:12 +0200

    v0.37: add prop.onCheckedChange

--- src/Properties.fs
+++ src/Properties.fs
@@ -162,6 +162,8 @@ type prop =
     /// Same as `onChange` but let's you deal with the text changed from the `input` element directly
     /// instead of extracting it from the event arguments.
     static member inline onTextChange (handler: string -> unit) = Interop.mkAttr "onChange" (fun (ev: Event) -> handler (!!ev.target?value))
+    /// Same as `onChange` but let's you deal with the `checked` value that has changed from the `input` element directly instead of extracting it from the event arguments.
+    static member inline onCheckedChange (handler: string -> unit) = Interop.mkAttr "onChange" (fun (ev: Event) -> handler (!!ev.target?``checked``))
     static member inline onInput (handler: Event -> unit) = Interop.mkAttr "onInput" handler
     static member inline onSubmit (handler: Event -> unit) = Interop.mkAttr "onSubmit" handler
     static member inline onReset (handler: Event -> unit) = Interop.mkAttr "onReset" handler
