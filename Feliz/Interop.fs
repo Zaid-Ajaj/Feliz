74be3aba083f4c855c5d48037a4cd42c6dc3d1c5
Zaid <zaid.naom@gmail.com>
2019-12-10 14:45:43 +0100

    v0.68.10: Improve generated bundle size by turning off the inlining of mkAttr and mkStyle

--- Feliz/Interop.fs
+++ Feliz/Interop.fs
@@ -9,7 +9,7 @@ open Feliz.ReactApi
 module Interop =
     let reactApi : IReactApi = importDefault "react"
     let reactElement (name: string) (props: 'a) : ReactElement = import "createElement" "react"
-    let inline mkAttr (key: string) (value: obj) : IReactProperty = unbox (key, value)
-    let inline mkStyle (key: string) (value: obj) : IStyleAttribute = unbox (key, value)
+    let mkAttr (key: string) (value: obj) : IReactProperty = unbox (key, value)
+    let mkStyle (key: string) (value: obj) : IStyleAttribute = unbox (key, value)
     let inline createElement name (properties: IReactProperty list) : ReactElement =
         reactElement name (createObj !!properties)
