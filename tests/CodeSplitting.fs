c39cfc856b12cf3a2163d2a4666c94c269cbdb4b
Shmew <Shmeww@gmail.com>
2020-05-05 12:29:28 -0500

    Add tests

---
+++ tests/CodeSplitting.fs
@@ -0,0 +1,12 @@
+﻿module CodeSplitting
+
+open Fable.Core.JsInterop
+open Feliz
+
+let myCodeSplitComponent = React.functionComponent(fun () ->
+    Html.div [
+        prop.testId "async-load"
+        prop.text "Loaded"
+    ])
+
+exportDefault myCodeSplitComponent
