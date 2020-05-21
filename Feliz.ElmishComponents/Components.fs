4a68683e5b46277acf5dcbbbbe9f0a16179592bd
Shmew <Shmeww@gmail.com>
2020-05-21 14:05:36 -0500

    Add useElmish hook and fix key/naming

--- Feliz.ElmishComponents/Components.fs
+++ Feliz.ElmishComponents/Components.fs
@@ -62,53 +62,76 @@ module ElmishComponentExtensions =
         | :? System.IDisposable as disposable -> Some disposable
         | _ -> None
 
-    [<EditorBrowsable(EditorBrowsableState.Never)>]
-    let elmishComponent<'State,'Msg> = React.memo(fun (input: ElmishComponentProps<'State,'Msg>) ->
-        let state = React.useRef(fst input.Initial)
-        let ring = React.useRef(RingBuffer(10))
-        let reentered = React.useRef(false)
-        let childState, setChildState = React.useState(fst input.Initial)
-        let setChildState () = JS.setTimeout(fun () -> setChildState state.current) 0 |> ignore
+    type React with
+        static member useElmish<'State,'Msg> (init: 'State * Cmd<'Msg>, update: 'Msg -> 'State -> 'State * Cmd<'Msg>) =
+            let state = React.useRef(fst init)
+            let ring = React.useRef(RingBuffer(10))
+            let reentered = React.useRef(false)
+            let childState, setChildState = React.useState(fst init)
+            let setChildState () = JS.setTimeout(fun () -> setChildState state.current) 0 |> ignore
 
-        let token = React.useRef(new System.Threading.CancellationTokenSource())
+            let token = React.useRef(new System.Threading.CancellationTokenSource())
     
-        let rec dispatch (msg: 'Msg) =
-            promise {
-                if reentered.current then
-                    ring.current.Push msg
-                else
-                    reentered.current <- false
-                    let mutable nextMsg = Some msg
+            let rec dispatch (msg: 'Msg) =
+                promise {
+                    if reentered.current then
+                        ring.current.Push msg
+                    else
+                        reentered.current <- false
+                        let mutable nextMsg = Some msg
 
-                    while nextMsg.IsSome && not (token.current.IsCancellationRequested) do
-                        let msg = nextMsg.Value
-                        let (state', cmd') = input.Update msg state.current
-                        cmd' |> List.iter (fun sub -> sub dispatch)
-                        nextMsg <- ring.current.Pop()
-                        state.current <- state'
-                        setChildState()
-                    reentered.current <- false
-            }
-            |> Promise.start
+                        while nextMsg.IsSome && not (token.current.IsCancellationRequested) do
+                            let msg = nextMsg.Value
+                            let (state', cmd') = update msg state.current
+                            cmd' |> List.iter (fun sub -> sub dispatch)
+                            nextMsg <- ring.current.Pop()
+                            state.current <- state'
+                            setChildState()
+                        reentered.current <- false
+                }
+                |> Promise.start
 
-        let dispatch = React.useCallbackRef(dispatch)
+            let dispatch = React.useCallbackRef(dispatch)
 
-        React.useEffectOnce(fun () ->
-            React.createDisposable <| fun () ->
-                token.current.Cancel()
-                getDisposable state.current
-                |> Option.iter (fun o -> o.Dispose())
-                token.current.Dispose())
+            React.useEffectOnce(fun () ->
+                React.createDisposable <| fun () ->
+                    token.current.Cancel()
+                    getDisposable state.current
+                    |> Option.iter (fun o -> o.Dispose())
+                    token.current.Dispose())
 
-        React.useEffectOnce(fun () ->
-            snd input.Initial
-            |> List.iter (fun sub -> sub dispatch))
-            
-        React.useEffect(fun () -> ring.current.Pop() |> Option.iter dispatch)
+            React.useEffectOnce(fun () ->
+                snd init
+                |> List.iter (fun sub -> sub dispatch))
+                
+            React.useEffect(fun () -> ring.current.Pop() |> Option.iter dispatch)
 
-        input.Render childState dispatch)
+            (childState, dispatch)
+
+        static member inline useElmish<'State,'Msg> (init: 'State, update: 'Msg -> 'State -> 'State * Cmd<'Msg>) =
+            React.useElmish<'State,'Msg>((init, Cmd.none), update)
+
+        static member inline useElmis<'State,'Msg>h (init: 'State * Cmd<'Msg>, update: 'Msg -> 'State -> 'State) =
+            React.useElmish<'State,'Msg>(init, (fun msg state -> update msg state, Cmd.none))
+
+        static member inline useElmish<'State,'Msg> (init: 'State, update: 'Msg -> 'State -> 'State) =
+            React.useElmish<'State,'Msg>((init, Cmd.none), (fun msg state -> update msg state, Cmd.none))
 
     type React with
+        //[<EditorBrowsable(EditorBrowsableState.Never)>]
+        static member inline elmishComponentInner<'State,'Msg> (name: string) =
+            React.memo(name, (fun (input: ElmishComponentProps<'State,'Msg>) ->
+                let childState, dispatch = React.useElmish(input.Initial, input.Update)
+
+                input.Render childState dispatch
+            ))
+        static member inline elmishComponentInner<'State,'Msg> (name: string, key: string) =
+            React.memo(name, (fun (input: ElmishComponentProps<'State,'Msg>) ->
+                let childState, dispatch = React.useElmish(input.Initial, input.Update)
+
+                input.Render childState dispatch
+            ), withKey = (fun _ -> key))
+
         /// Creates a standalone React component using an Elmish dispatch loop
         static member inline elmishComponent(name: string, init: 'Model * Cmd<'Msg>, update: 'Msg -> 'Model -> 'Model * Cmd<'Msg>, render: 'Model -> ('Msg -> unit) -> ReactElement, ?key: string) =
             let fullKey =
@@ -116,7 +139,12 @@ module ElmishComponentExtensions =
                 | None -> name
                 | Some key -> name + "-" + key
 
-            elmishComponent { Initial = init; Update = update; Render = render; key = fullKey }
+            React.elmishComponentInner<'Model,'Msg>(name, fullKey) { 
+                Initial = init
+                Update = update
+                Render = render
+                key = fullKey 
+            }
 
         /// Creates a standalone React component using an Elmish dispatch loop
         static member inline elmishComponent(name: string, init: 'Model, update: 'Msg -> 'Model -> 'Model * Cmd<'Msg>, render: 'Model -> ('Msg -> unit) -> ReactElement, ?key: string)  =
@@ -125,7 +153,12 @@ module ElmishComponentExtensions =
                 | None -> name
                 | Some key -> name + "-" + key
 
-            elmishComponent { Initial = init, Cmd.none; Update = update; Render = render; key = fullKey }
+            React.elmishComponentInner<'Model,'Msg>(name, fullKey) { 
+                Initial = (init, Cmd.none)
+                Update = update
+                Render = render
+                key = fullKey 
+            }
     
         /// Creates a standalone React component using an Elmish dispatch loop
         static member inline elmishComponent(name: string, init: 'Model, update: 'Msg -> 'Model -> 'Model, render: 'Model -> ('Msg -> unit) -> ReactElement, ?key: string) =
@@ -134,11 +167,11 @@ module ElmishComponentExtensions =
                 | None -> name
                 | Some key -> name + "-" + key
 
-            elmishComponent
-                { Initial = init, Cmd.none;
-                  Update = fun msg state -> update msg state, Cmd.none;
-                  Render = render
-                  key = fullKey }
+            React.elmishComponentInner<'Model,'Msg>(name, fullKey) { 
+                Initial = (init, Cmd.none)
+                Update = (fun msg state -> update msg state, Cmd.none)
+                Render = render; key = fullKey 
+            }
     
         /// Creates a standalone React component using an Elmish dispatch loop
         static member inline elmishComponent(name: string, init: 'Model * Cmd<'Msg>, update: 'Msg -> 'Model -> 'Model, render: 'Model -> ('Msg -> unit) -> ReactElement, ?key: string) =
@@ -147,8 +180,8 @@ module ElmishComponentExtensions =
                 | None -> name
                 | Some key -> name + "-" + key
 
-            elmishComponent
-                { Initial = init;
-                  Update = fun msg state -> update msg state, Cmd.none;
-                  Render = render
-                  key = fullKey }
+            React.elmishComponentInner<'Model,'Msg>(name, fullKey) { 
+                Initial = init
+                Update = (fun msg state -> update msg state, Cmd.none)
+                Render = render; key = fullKey 
+            }
