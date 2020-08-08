0d1cd3b8ba30c50c94ed1e7c49a1e8289463e0bf
Shmew <Shmeww@gmail.com>
2020-08-08 00:40:05 -0500

    capitalization

--- Feliz/Styles.fs
+++ Feliz/Styles.fs
@@ -974,7 +974,7 @@ module style =
         /// Inherits this property from its parent element.
         static member inline inheritFromParent = Interop.mkStyle "flexWrap" "inherit"
 
-    /// places an element on the left or right side of its container, allowing text and 
+    /// Places an element on the left or right side of its container, allowing text and 
     /// inline elements to wrap around it. The element is removed from the normal flow 
     /// of the page, though still remaining a part of the flow (in contrast to absolute 
     /// positioning).
