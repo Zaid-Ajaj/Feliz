b4d626b5796a77057703b74e87e92f10d0bae3c1
Zaid <zaid.naom@gmail.com>
2020-09-01 02:35:28 +0200

    Updates

--- 1.bundle.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{663:function(n,e,t){"use strict";t.r(e),t.d(e,"myCodeSplitComponent",(function(){return i}));var o=t(0),c=t(1),d=t(4),s=t(7);const i=Object(s.e)((function(){const n=new c.b(Object(o.a)("children","I was loaded asynchronously!"),new c.b);return Object(o.d)("div",Object(d.j)(n,0))}));e.default=i}}]);
