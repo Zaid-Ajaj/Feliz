ca1f6c5c96ec0bb58af9b699e9cc3d18a33ffcea
Zaid <zaid.naom@gmail.com>
2020-11-01 18:14:26 +0100

    Feliz v1.16.2 Only implement [ReactComponent], hide [PrimitiveElement] for now for another release

--- Feliz.CompilerPlugins/PrimitiveElement.fs
+++ Feliz.CompilerPlugins/PrimitiveElement.fs
@@ -3,16 +3,16 @@ namespace Feliz
 open Fable
 open Fable.AST
 
-type PrimitiveElementAttribute(tagName:string) =
-    inherit MemberDeclarationPluginAttribute()
-    override _.FableMinimumVersion = "3.0"
-
-    override _.TransformCall(logger, memb, expr) =
-        match expr with
-        | Fable.Call(callee, info, typeInfo, range) when info.Args.Length = 1 ->
-            let element = AstUtils.makeStrConst tagName
-            AstUtils.makeCall (AstUtils.makeImport "createElement" "react") [element; AstUtils.nullValue; info.Args.[0]]
-        | _ ->
-            expr
-
-    override this.Transform(logger, decl) = decl
+//type PrimitiveElementAttribute(tagName:string) =
+//    inherit MemberDeclarationPluginAttribute()
+//    override _.FableMinimumVersion = "3.0"
+//
+//    override _.TransformCall(logger, memb, expr) =
+//        match expr with
+//        | Fable.Call(callee, info, typeInfo, range) when info.Args.Length = 1 ->
+//            let element = AstUtils.makeStrConst tagName
+//            AstUtils.makeCall (AstUtils.makeImport "createElement" "react") [element; AstUtils.nullValue; info.Args.[0]]
+//        | _ ->
+//            expr
+//
+//    override this.Transform(logger, decl) = decl
