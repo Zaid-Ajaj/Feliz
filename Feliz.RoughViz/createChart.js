bcae9776562fe97349d00108a875d3b85a8c619d
Stuart Lang <stuart.b.lang@gmail.com>
2020-11-25 01:02:07 +0000

    Update roughViz and add pie charts

--- Feliz.RoughViz/createChart.js
+++ Feliz.RoughViz/createChart.js
@@ -1,5 +1,7 @@
-import RoughViz from 'rough-viz/dist/roughviz.min'
+import * as RoughViz from '@inocan/rough-viz'
 
 export const createBarChart = (config) => new RoughViz.Bar(config)
 
 export const createHorizontalBarChart = (config) => new RoughViz.BarH(config)
+
+export const createPieChart = (config) => new RoughViz.Pie(config)
