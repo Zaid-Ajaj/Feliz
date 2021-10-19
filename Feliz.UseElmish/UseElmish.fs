13239ab7199df4a1af30d2b12c7f7b426a229b53
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-10-19 14:24:57 +0900

    Use Elmish.Program in useElmish

--- Feliz.UseElmish/UseElmish.fs
+++ Feliz.UseElmish/UseElmish.fs
@@ -2,117 +2,58 @@ namespace Feliz.UseElmish
 
 open Feliz
 open Elmish
-open Fable.Core
 
-[<Struct>]
-type internal RingState<'item> =
-    | Writable of wx:'item array * ix:int
-    | ReadWritable of rw:'item array * wix:int * rix:int
+type private ElmishObservable<'State, 'Msg>() =
+    let mutable state: 'State option = None
+    let mutable listener: ('State -> unit) option = None
+    let mutable dispatcher: ('Msg -> unit) option = None
 
-type internal RingBuffer<'item>(size) =
-    let doubleSize ix (items: 'item array) =
-        seq { yield! items |> Seq.skip ix
-              yield! items |> Seq.take ix
-              for _ in 0..items.Length do
-                yield Unchecked.defaultof<'item> }
-        |> Array.ofSeq
+    member _.Value = state
 
-    let mutable state : 'item RingState =
-        Writable (Array.zeroCreate (max size 10), 0)
+    member _.SetState (model: 'State) (dispatch: 'Msg -> unit) =
+        state <- Some model
+        dispatcher <- Some dispatch
+        match listener with
+        | None -> ()
+        | Some listener -> listener model
 
-    member _.Pop() =
-        match state with
-        | ReadWritable (items, wix, rix) ->
-            let rix' = (rix + 1) % items.Length
-            match rix' = wix with
-            | true ->
-                state <- Writable(items, wix)
-            | _ ->
-                state <- ReadWritable(items, wix, rix')
-            Some items.[rix]
-        | _ ->
-            None
+    member _.Dispatch(msg) =
+        match dispatcher with
+        | None -> () // Error?
+        | Some dispatch -> dispatch msg
 
-    member _.Push (item:'item) =
-        match state with
-        | Writable (items, ix) ->
-            items.[ix] <- item
-            let wix = (ix + 1) % items.Length
-            state <- ReadWritable(items, wix, ix)
-        | ReadWritable (items, wix, rix) ->
-            items.[wix] <- item
-            let wix' = (wix + 1) % items.Length
-            match wix' = rix with
-            | true ->
-                state <- ReadWritable(items |> doubleSize rix, items.Length, 0)
-            | _ ->
-                state <- ReadWritable(items, wix', rix)
+    member _.Subscribe(f) =
+        match listener with
+        | Some _ -> ()
+        | None -> listener <- Some f
 
 [<AutoOpen>]
 module UseElmishExtensions =
-    let inline internal getDisposable (record: 'State) =
-        match box record with
-        | :? System.IDisposable as disposable -> Some disposable
-        | _ -> None
-
     type React with
         [<Hook>]
-        static member useElmish<'State,'Msg> (init: 'State * Cmd<'Msg>, update: 'Msg -> 'State -> 'State * Cmd<'Msg>, dependencies: obj[]) =
-            let state = React.useRef(fst init)
-            let ring = React.useRef(RingBuffer(10))
-            let childState, setChildState = React.useState(fst init)
-            let token = React.useCancellationToken()
-            let setChildState () =
-                JS.setTimeout(fun () ->
-                    if not token.current.IsCancellationRequested then
-                        setChildState state.current
-                ) 0 |> ignore
+        static member useElmish(program: unit -> Program<unit, 'State, 'Msg, unit>) =
+            // Don't use useMemo here because React doesn't guarantee it won't recreate it again
+            let obs, _ = React.useState(fun () -> ElmishObservable())
 
-            let rec dispatch (msg: 'Msg) =
-                promise {
-                    let mutable nextMsg = Some msg
+            let state, setState = React.useState(fun () ->
+                program()
+                |> Program.withSetState obs.SetState
+                |> Program.run
 
-                    while nextMsg.IsSome && not (token.current.IsCancellationRequested) do
-                        let msg = nextMsg.Value
-                        let (state', cmd') = update msg state.current
-                        cmd' |> List.iter (fun sub -> sub dispatch)
-                        nextMsg <- ring.current.Pop()
-                        state.current <- state'
-                        setChildState()
-                }
-                |> Promise.start
+                match obs.Value with
+                | None -> failwith "Elmish program has not initialized"
+                | Some v -> v)
 
-            let dispatch = React.useCallbackRef(dispatch)
-
-            React.useEffect((fun () ->
+            React.useEffectOnce(fun () ->
                 React.createDisposable(fun () ->
-                    getDisposable state.current
-                    |> Option.iter (fun o -> o.Dispose())
-                )
-            ), dependencies)
+                        match box state with
+                        | :? System.IDisposable as disp -> disp.Dispose()
+                        | _ -> ()))
 
-            React.useEffect((fun () ->
-                state.current <- fst init
-                setChildState()
+            obs.Subscribe(setState)
+            state, obs.Dispatch
 
-                snd init
-                |> List.iter (fun sub -> sub dispatch)
-            ), dependencies)
-
-            React.useEffect(fun () -> ring.current.Pop() |> Option.iter dispatch)
-
-            (childState, dispatch)
-
-        [<Hook>]
-        static member inline useElmish<'State,'Msg> (init: 'State * Cmd<'Msg>, update: 'Msg -> 'State -> 'State * Cmd<'Msg>) =
-            React.useElmish(init, update, [||])
-
-        [<Hook>]
-        static member useElmish<'State,'Msg> (init: unit -> 'State * Cmd<'Msg>, update: 'Msg -> 'State -> 'State * Cmd<'Msg>, dependencies: obj[]) =
-            let init = React.useMemo(init, dependencies)
-
-            React.useElmish(init, update, dependencies)
-
-        [<Hook>]
-        static member inline useElmish<'State,'Msg> (init: unit -> 'State * Cmd<'Msg>, update: 'Msg -> 'State -> 'State * Cmd<'Msg>) =
-            React.useElmish(init, update, [||])
+        static member useElmish(update, init) =
+            React.useElmish(fun () ->
+                let view _ _ = ()
+                Program.mkProgram init update view)
