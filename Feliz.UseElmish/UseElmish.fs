320a749299b0fbb78e5bdb957647a288755182ca
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-10-28 22:21:19 +0900

    UseElmish with dependencies

--- Feliz.UseElmish/UseElmish.fs
+++ Feliz.UseElmish/UseElmish.fs
@@ -3,65 +3,89 @@ namespace Feliz.UseElmish
 open Feliz
 open Elmish
 
-type private ElmishObservable<'State, 'Msg>() =
-    let mutable state: 'State option = None
-    let mutable listener: ('State -> unit) option = None
-    let mutable dispatcher: ('Msg -> unit) option = None
-
-    member _.Value = state
-
-    member _.SetState (model: 'State) (dispatch: 'Msg -> unit) =
-        state <- Some model
-        dispatcher <- Some dispatch
-        match listener with
-        | None -> ()
-        | Some listener -> listener model
-
-    member _.Dispatch(msg) =
-        match dispatcher with
-        | None -> () // Error?
-        | Some dispatch -> dispatch msg
-
-    member _.Subscribe(f) =
-        match listener with
-        | Some _ -> ()
-        | None -> listener <- Some f
-
 [<AutoOpen>]
 module UseElmishExtensions =
+    type private ElmishObservable<'Model, 'Msg>() =
+        let mutable hasDisposedOnce = false
+        let mutable state: 'Model option = None
+        let mutable listener: ('Model -> unit) option = None
+        let mutable dispatcher: ('Msg -> unit) option = None
+
+        member _.Value = state
+        member _.HasDisposedOnce = hasDisposedOnce
+
+        member _.SetState (model: 'Model) (dispatch: 'Msg -> unit) =
+            state <- Some model
+            dispatcher <- Some dispatch
+            match listener with
+            | None -> ()
+            | Some listener -> listener model
+
+        member _.Dispatch(msg) =
+            match dispatcher with
+            | None -> () // Error?
+            | Some dispatch -> dispatch msg
+
+        member _.Subscribe(f) =
+            match listener with
+            | Some _ -> ()
+            | None -> listener <- Some f
+
+        /// Disposes state (and dispatcher) but keeps subscription
+        member _.DisposeState() =
+            match state with
+            | Some state ->
+                match box state with
+                | :? System.IDisposable as disp -> disp.Dispose()
+                | _ -> ()
+            | _ -> ()
+            dispatcher <- None
+            state <- None
+            hasDisposedOnce <- true
+
+    let private runProgram (program: unit -> Program<'Arg, 'Model, 'Msg, unit>) (arg: 'Arg) (obs: ElmishObservable<'Model, 'Msg>) () =
+        program()
+        |> Program.withSetState obs.SetState
+        |> Program.runWith arg
+
+        match obs.Value with
+        | None -> failwith "Elmish program has not initialized"
+        | Some v -> v        
+
+    let disposeState (state: obj) =
+        match box state with
+        | :? System.IDisposable as disp -> disp.Dispose()
+        | _ -> ()
+
     type React with
         [<Hook>]
-        static member useElmish(program: unit -> Program<'arg, 'State, 'Msg, unit>, arg: 'arg) =
+        static member useElmish(program: unit -> Program<'Arg, 'Model, 'Msg, unit>, arg: 'Arg, ?dependencies: obj array) =
             // Don't use useMemo here because React doesn't guarantee it won't recreate it again
-            let obs, _ = React.useState(fun () -> ElmishObservable())
+            let obs, _ = React.useState(fun () -> ElmishObservable<'Model, 'Msg>())
 
-            let state, setState = React.useState(fun () ->
-                program()
-                |> Program.withSetState obs.SetState
-                |> Program.runWith arg
+            let state, setState = React.useState(runProgram program arg obs)
 
-                match obs.Value with
-                | None -> failwith "Elmish program has not initialized"
-                | Some v -> v)
-
-            React.useEffectOnce(fun () ->
-                React.createDisposable(fun () ->
-                        match box state with
-                        | :? System.IDisposable as disp -> disp.Dispose()
-                        | _ -> ()))
+            React.useEffect((fun () ->
+                if obs.HasDisposedOnce then
+                    runProgram program arg obs () |> setState
+                React.createDisposable(obs.DisposeState)
+            ), defaultArg dependencies [||])
 
             obs.Subscribe(setState)
             state, obs.Dispatch
 
         [<Hook>]
-        static member useElmish(program: unit -> Program<unit, 'State, 'Msg, unit>) =
-            React.useElmish(program, ())
+        static member useElmish(program: unit -> Program<unit, 'Model, 'Msg, unit>, ?dependencies: obj array) =
+            React.useElmish(program, (), ?dependencies=dependencies)
 
-        static member useElmish(init, update, arg, ?dependencies: obj array) =
-            React.useElmish((fun () -> Program.mkProgram init update (fun _ _ -> ())), arg)
+        [<Hook>]
+        static member useElmish(init: 'Arg -> 'Model * Cmd<'Msg>, update: 'Msg -> 'Model -> 'Model * Cmd<'Msg>, arg: 'Arg, ?dependencies: obj array) =
+            React.useElmish((fun () -> Program.mkProgram init update (fun _ _ -> ())), arg, ?dependencies=dependencies)
 
-        static member useElmish(init, update, ?dependencies: obj array) =
-            React.useElmish(fun () -> Program.mkProgram init update (fun _ _ -> ()))
+        [<Hook>]
+        static member useElmish(init: unit -> 'Model * Cmd<'Msg>, update: 'Msg -> 'Model -> 'Model * Cmd<'Msg>, ?dependencies: obj array) =
+            React.useElmish((fun () -> Program.mkProgram init update (fun _ _ -> ())), ?dependencies=dependencies)
 
-        static member useElmish(initial, update, ?dependencies: obj array) =
-            React.useElmish(fun () -> Program.mkProgram (fun () -> initial) update (fun _ _ -> ()))
+        [<Hook>]
+        static member useElmish(init: 'Model * Cmd<'Msg>, update: 'Msg -> 'Model -> 'Model * Cmd<'Msg>, ?dependencies: obj array) =
+            React.useElmish((fun () -> Program.mkProgram (fun () -> init) update (fun _ _ -> ())), ?dependencies=dependencies)
