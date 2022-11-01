11695e5b0eada5d3efcfdc3ef9a0ceb5ffdc5674
Linus Vettiger <lv@vlot.ch>
2022-11-01 22:59:27 +0100

    Fix TransformOrigin contents

--- Feliz/TransformOrigin.fs
+++ Feliz/TransformOrigin.fs
@@ -1,14 +1,17 @@
 namespace Feliz
 
+open Fable.Core
+open Feliz.Styles
+
 [<Erase>]
 type origin =
-    static member inline left : ITransformOrigin = "left"
-    static member inline center : ITransformOrigin = "center"
-    static member inline right : ITransformOrigin = "right"
-    static member inline top : ITransformOrigin = "top"
-    static member inline bottom : ITransformOrigin = "bottom"
+    static member inline left : ITransformOrigin = unbox "left"
+    static member inline center : ITransformOrigin = unbox "center"
+    static member inline right : ITransformOrigin = unbox "right"
+    static member inline top : ITransformOrigin = unbox "top"
+    static member inline bottom : ITransformOrigin = unbox "bottom"
 
     static member inline percentage (value: int) : ITransformOrigin =
-        unbox<string> value + "%"
+        unbox (unbox<string> value + "%")
     static member inline percentage (value: float) : ITransformOrigin =
-        unbox<string> value + "%" 
+        unbox (unbox<string> value + "%") 
